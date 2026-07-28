8a928530bc01d769c09cc204546679e8b1139712
sgaeric6 <sgaeric6@gmail.com>
2026-07-28 11:04:17 +0100

    Create Dashboard.jsx

---
+++ support/src/pages/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <div style={{ padding: "30px" }}>
+      <h1>Support Dashboard</h1>
+
+      <ul>
+        <li>Complaints</li>
+        <li>Sell Requests</li>
+        <li>Private Chats</li>
+        <li>Ratings</li>
+      </ul>
+    </div>
+  );
+}
