cfe1e9e35f99afde6404b093c7b6b9a7dc856691
sgaeric6 <sgaeric6@gmail.com>
2026-07-28 05:47:40 +0100

    Create index.js

---
+++ server/src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    name: "ZenXStore API",
+    status: "Online",
+    version: "1.0.0"
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
