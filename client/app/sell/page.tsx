d73fe2d25ca64cf9345f7e70258800b375a4edb8
sgaeric6 <sgaeric6@gmail.com>
2026-07-28 10:33:35 +0100

    Add SellPage component for selling accounts

---
+++ client/app/sell/page.tsx
@@ -0,0 +1,11 @@
+export default function SellPage() {
+  return (
+    <main style={{ padding: 30 }}>
+      <h1>Sell Your Free Fire Account</h1>
+
+      <button>
+        Submit Account
+      </button>
+    </main>
+  );
+}
