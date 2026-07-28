486eaff517ea76a3540d34ee334278987edb8894
sgaeric6 <sgaeric6@gmail.com>
2026-07-28 11:07:53 +0100

    Create Chats.jsx

---
+++ support/src/pages/Chats.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Chats() {
+  return (
+    <div>
+
+      <h1>Private Chat</h1>
+
+      <textarea
+        rows="8"
+        placeholder="Chat here..."
+      />
+
+      <br /><br />
+
+      <button>Send</button>
+
+    </div>
+  );
+}
