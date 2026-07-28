7f362bb7bc4a8bd599cb2f8a5679409484607cfc
sgaeric6 <sgaeric6@gmail.com>
2026-07-28 10:29:35 +0100

    Create page.tsx

---
+++ client/app/client/app/login/page.tsx
@@ -0,0 +1,22 @@
+export default function LoginPage() {
+  return (
+    <main style={{ padding: 30 }}>
+      <h1>Login</h1>
+
+      <input placeholder="Email" />
+
+      <br />
+      <br />
+
+      <input
+        type="password"
+        placeholder="Password"
+      />
+
+      <br />
+      <br />
+
+      <button>Login</button>
+    </main>
+  );
+}
