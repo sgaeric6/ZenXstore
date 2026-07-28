b13ec2e842191bc35fb6c51831ee5c35b76926a2
sgaeric6 <sgaeric6@gmail.com>
2026-07-28 10:52:48 +0100

    Create Support.jsx

---
+++ admin/src/pages/Support.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Support() {
+  return (
+    <div>
+
+      <h1>Support Staff</h1>
+
+      <button>Approve Support</button>
+
+      <button>Reject Support</button>
+
+    </div>
+  );
+}
