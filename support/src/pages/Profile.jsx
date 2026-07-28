a2d21743b57a172df46c3b14a5413bff9acd77e1
sgaeric6 <sgaeric6@gmail.com>
2026-07-28 11:10:07 +0100

    Create Profile.jsx

---
+++ support/src/pages/Profile.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Profile() {
+  return (
+    <div>
+
+      <h1>Support Profile</h1>
+
+      <p>Status: Pending Admin Approval</p>
+
+      <button>Edit Profile</button>
+
+    </div>
+  );
+}
