d451bd662b7ee2ea90474794da8e580e722df27b
sgaeric6 <sgaeric6@gmail.com>
2026-07-28 10:59:17 +0100

    Add Analytics page component

---
+++ admin/src/pages/Analytics.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Analytics() {
+  return (
+    <div>
+
+      <h1>Analytics</h1>
+
+      <p>Revenue</p>
+
+      <p>Orders</p>
+
+      <p>Users</p>
+
+    </div>
+  );
+}
