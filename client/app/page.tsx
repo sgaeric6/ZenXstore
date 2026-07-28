69bba98c2ff2633f34c6e4c4bb45ec1ae0023867
sgaeric6 <sgaeric6@gmail.com>
2026-07-28 05:35:10 +0100

    Create page.tsx

---
+++ client/app/page.tsx
@@ -0,0 +1,23 @@
+export default function HomePage() {
+  return (
+    <main
+      style={{
+        padding: "40px",
+        textAlign: "center",
+        fontFamily: "Arial"
+      }}
+    >
+      <h1>🔥 ZenXStore</h1>
+
+      <p>Professional Free Fire Account Marketplace</p>
+
+      <br />
+
+      <button>Buy Account</button>
+
+      <button style={{ marginLeft: 10 }}>
+        Sell Account
+      </button>
+    </main>
+  );
+}
