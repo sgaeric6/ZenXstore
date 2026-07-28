85394ec6c54d2a3b4a427aff9a836cc2c7c71028
sgaeric6 <sgaeric6@gmail.com>
2026-07-28 10:58:32 +0100

    Create Transactions.jsx

---
+++ admin/src/pages/Transactions.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Transactions() {
+  return (
+    <div>
+
+      <h1>Transactions</h1>
+
+      <p>Transaction history will appear here.</p>
+
+    </div>
+  );
+}
