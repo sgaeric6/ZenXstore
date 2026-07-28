1b006648c2fde9aaa5af1f89a7bf342cf0eb4777
sgaeric6 <sgaeric6@gmail.com>
2026-07-28 11:11:53 +0100

    Add Navbar component for ZenXStore Support

---
+++ support/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav
+      style={{
+        padding: "20px",
+        borderBottom: "1px solid #ddd"
+      }}
+    >
+      <h2>ZenXStore Support</h2>
+    </nav>
+  );
+}
