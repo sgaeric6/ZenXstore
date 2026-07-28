7d1ce086f4550943331c366d5b0a3f33662f2bc0
sgaeric6 <sgaeric6@gmail.com>
2026-07-28 10:34:16 +0100

    Create Dashboard component for user interface

---
+++ client/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+export default function Dashboard() {
+  return (
+    <main style={{ padding: 30 }}>
+      <h1>User Dashboard</h1>
+
+      <ul>
+        <li>My Orders</li>
+        <li>Transactions</li>
+        <li>Support</li>
+        <li>Profile</li>
+      </ul>
+    </main>
+  );
+}
