01d19c9b92743b33e193181c7824b7f84075bf9f
sgaeric6 <sgaeric6@gmail.com>
2026-07-28 10:53:37 +0100

    Create Refunds.jsx

---
+++ admin/src/pages/Refunds.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Refunds() {
+  return (
+    <div>
+
+      <h1>Refund Requests</h1>
+
+      <button>Approve Refund</button>
+
+      <button>Reject Refund</button>
+
+    </div>
+  );
+}
