ed93122a963c91146c2b5e6441f38b4cc59267c6
sgaeric6 <sgaeric6@gmail.com>
2026-07-28 10:38:15 +0100

    Create page.tsx

---
+++ client/app/profile/page.tsx
@@ -0,0 +1,13 @@
+export default function ProfilePage() {
+  return (
+    <main style={{ padding: 30 }}>
+      <h1>My Profile</h1>
+
+      <p>Name</p>
+      <p>Email</p>
+      <p>Verified Badge</p>
+
+      <button>Edit Profile</button>
+    </main>
+  );
+}
