a47148876fcb13fba81f02b307de7c54a02e953a
sgaeric6 <sgaeric6@gmail.com>
2026-07-28 11:05:55 +0100

    Create Complaints.jsx

---
+++ support/src/pages/Complaints.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Complaints() {
+  return (
+    <div>
+
+      <h1>Complaints</h1>
+
+      <button>Open Complaint</button>
+
+      <button>Resolve</button>
+
+    </div>
+  );
+}
