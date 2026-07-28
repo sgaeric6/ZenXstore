2b4be3266cc9f32634c1b397fafb5d47e68472f6
sgaeric6 <sgaeric6@gmail.com>
2026-07-28 11:06:49 +0100

    Create SellRequests.jsx

---
+++ support/src/pages/SellRequests.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function SellRequests() {
+  return (
+    <div>
+
+      <h1>Sell Requests</h1>
+
+      <button>Bought</button>
+
+      <button>Declined</button>
+
+    </div>
+  );
+}
