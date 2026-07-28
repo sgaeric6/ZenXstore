24158c0d66ed8a3985831d9c818bef4c72f6b4b9
sgaeric6 <sgaeric6@gmail.com>
2026-07-28 10:40:28 +0100

    Create page.tsx

---
+++ client/app/support/page.tsx
@@ -0,0 +1,17 @@
+export default function SupportPage() {
+  return (
+    <main style={{ padding: 30 }}>
+      <h1>Support Chat</h1>
+
+      <textarea
+        placeholder="Type your message..."
+        rows={6}
+      />
+
+      <br />
+      <br />
+
+      <button>Send</button>
+    </main>
+  );
+}
