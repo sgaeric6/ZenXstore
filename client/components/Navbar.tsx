bb1c291da4b422b10c39aef3dc4520b4c90a600e
sgaeric6 <sgaeric6@gmail.com>
2026-07-30 13:29:53 +0100

    Update Navbar component with new links and styling

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,16 +1,29 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <nav
       style={{
         display: "flex",
         justifyContent: "space-between",
-        padding: "20px"
+        padding: "20px 50px",
+        background: "#111827",
+        alignItems: "center"
       }}
     >
       <h2>ZenXStore</h2>
 
-      <div>
-        Home | Buy | Sell | Login
+      <div
+        style={{
+          display: "flex",
+          gap: "20px"
+        }}
+      >
+        <Link href="/">Home</Link>
+        <Link href="/buy">Buy</Link>
+        <Link href="/sell">Sell</Link>
+        <Link href="/dashboard">Dashboard</Link>
+        <Link href="/login">Login</Link>
       </div>
     </nav>
   );
