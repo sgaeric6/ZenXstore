a86bf00faa83136e5fceca99cd6f61eee8954c0e
sgaeric6 <sgaeric6@gmail.com>
2026-07-30 18:40:15 +0100

    Refactor Navbar component layout and links

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -5,36 +5,49 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <header className="navbar">
+
       <div className="navContainer">
 
         <Link href="/" className="logo">
-          <span className="logoGlow">Z</span>
-          <span>ZenXStore</span>
+
+          <div className="logoGlow">
+            ZX
+          </div>
+
+          <span>
+            Zen<span className="green">X</span>Store
+          </span>
+
         </Link>
 
         <nav className="navLinks">
+
           <Link href="/">Home</Link>
-          <Link href="/buy">Buy</Link>
+
+          <Link href="/marketplace">Marketplace</Link>
+
+          <Link href="/categories">Categories</Link>
+
           <Link href="/sell">Sell</Link>
-          <Link href="/listings">Marketplace</Link>
+
           <Link href="/support">Support</Link>
+
         </nav>
 
         <div className="navButtons">
-          <Link href="/login">
-            <button className="loginBtn">
-              Login
-            </button>
-          </Link>
 
-          <Link href="/signup">
-            <button className="signupBtn">
-              Get Started
-            </button>
-          </Link>
+          <button className="loginBtn">
+            Login
+          </button>
+
+          <button className="signupBtn">
+            Get Started
+          </button>
+
         </div>
 
       </div>
+
     </header>
   );
 }
