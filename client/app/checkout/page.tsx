42d3ef4a745573c04389bc72ab7bf90e8fe5558e
sgaeric6 <sgaeric6@gmail.com>
2026-08-05 06:50:05 +0100

    Update page.tsx

--- client/app/checkout/page.tsx
+++ client/app/checkout/page.tsx
@@ -1,1 +1,10 @@
+import { Suspense } from "react";
+import CheckoutClient from "../CheckoutClient";
 
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading checkout...</div>}>
+      <CheckoutClient />
+    </Suspense>
+  );
+}
